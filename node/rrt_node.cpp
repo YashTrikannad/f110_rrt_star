913c14b9546b3c6738e1dfa8c85d717e5634cebd
YashTrikannad <yash.trikannad@gmail.com>
2019-11-06 20:22:21 -0500

    add launch and config

--- node/rrt_node.cpp
+++ node/rrt_node.cpp
@@ -1,7 +1,6 @@
 // This file contains the node definition for RRT
 // Before you start, please read: https://arxiv.org/pdf/1105.1186.pdf
 
-
 #include "rrt/rrt.h"
 
 int main(int argc, char** argv) {
