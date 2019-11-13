f7b636e7aa24891695a0531c39c0474f181c383e
Sonitabh <41808387+Sonitabh@users.noreply.github.com>
2019-11-13 15:11:57 -0500

    Update rrt.cpp

--- src/rrt.cpp
+++ src/rrt.cpp
@@ -121,7 +121,7 @@ void RRT::scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
     const auto end = static_cast<int>(2*scan_msg->ranges.size()/3);
     const double angle_increment = scan_msg->angle_increment;
     double theta = scan_msg->angle_min + angle_increment*(start-1);
-	int Thresh = 10;	/////////// Threshold for obstacle inflation, add to params server
+    int Thresh = 10;	/////////// Threshold for obstacle inflation, add to params server
 
     for(int i=start; i<end; ++i)
     {
@@ -145,18 +145,18 @@ void RRT::scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg)
         {
             input_map_.data[index] = 100;
             new_obstacles_.emplace_back(index);
-			
-			//// INFLATION OF OBJECT
-			///////////////////////////////////////////////////////////////////////////////////////////////
-			for(int j=(index.x - Thresh); j < (index.x + Thresh); ++j){
-				for (int k=(index.y - Thresh); j < (index.x + Thresh); ++k)
-				{
-					input_map_.data[j,k] = 100;
-					new_obstacles_.emplace_back(index);
-				}
+            //// INFLATION OF OBJECT
+            ///////////////////////////////////////////////////////////////////////////////////////////////
+            for(int j=(index.x - Thresh); j < (index.x + Thresh); ++j)
+	    {
+                for (int k=(index.y - Thresh); j < (index.x + Thresh); ++k)
+		{
+		    input_map_.data[j,k] = 100;
+		    new_obstacles_.emplace_back(index);
+		}
 				
-			}
-			///////////////////////////////////////////////////////////////////////////////////////////////
+	    }
+	    ///////////////////////////////////////////////////////////////////////////////////////////////
         }
     }
 
